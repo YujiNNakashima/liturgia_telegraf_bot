4c44dee1849cb5f941fce9c388d899088689640a
Yuji <yujiricardo@gmail.com>
2021-11-30 15:20:08 -0300

    refactor: change a text

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => {
 
-  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.1`)
+  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.2`)
   const tz = 'America/Sao_Paulo'
 
   const job = new CronJob('0 4,9,12,15,18 * * *', function() {
