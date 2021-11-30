ef2c5737d3e14db29d2ce9985254aa3a020a4e60
Yuji <yujiricardo@gmail.com>
2021-11-30 18:19:53 -0300

    fix: timezone

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const {Telegraf} = require('telegraf') 
 const CronJob = require('cron').CronJob;
 const generateHour = require('./generateTextHour')
-const {  zonedTimeToUtc } = require('date-fns-tz')
+const {  utcToZonedTime  } = require('date-fns-tz')
 
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => {
 
-  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.2`)
+  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.3`)
   const tz = 'America/Sao_Paulo'
 
   const job = new CronJob('0 4,9,12,15,18 * * *', function() {
-    const currentHour = zonedTimeToUtc(new Date(), tz).getHours()
+    const currentHour = utcToZonedTime(new Date(), tz).getHours()
     const currentPrayer = generateHour(currentHour)
     ctx.reply(`${currentHour}h: rezar ${currentPrayer}`)
   }, null, true, tz);
