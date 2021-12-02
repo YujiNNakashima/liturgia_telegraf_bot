81687851fefb46012f893e7ac5a204d5ec908edc
Yuji <yujiricardo@gmail.com>
2021-12-02 09:26:30 -0300

    feat: enforce days of the week

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => {
 
-  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.4`)
+  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.5`)
   const tz = 'America/Sao_Paulo'
 
-  const job = new CronJob('0 4,9,12,15,18 * * *', function() {
+  const job = new CronJob('0 4,9,12,15 * * 0-7', function() {
     const currentHour = utcToZonedTime(new Date(), tz).getHours()
     const currentPrayer = generateHour(currentHour)
     ctx.reply(`${currentHour}h - rezar ${currentPrayer}`)
