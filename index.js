d8322560add7a83ae29ea90f8ede5021f64db1f7
Yuji <yujiricardo@gmail.com>
2021-12-02 18:03:23 -0300

    fix: 18h

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => {
 
-  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.6`)
+  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.7`)
   const tz = 'America/Sao_Paulo'
 
-  const job = new CronJob('0 4,9,12,15 * * 0-6', function() {
+  const job = new CronJob('0 4,9,12,15,18 * * 0-6', function() {
     const currentHour = utcToZonedTime(new Date(), tz).getHours()
     const currentPrayer = generateHour(currentHour)
     ctx.reply(`${currentHour}h - rezar ${currentPrayer}`)
