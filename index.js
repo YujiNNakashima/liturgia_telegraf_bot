f947f95982fde1cce62ae8a63b80d12158fefc1b
Yuji <yujiricardo@gmail.com>
2021-12-06 15:37:09 -0300

    feat: add another cron

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {Telegraf} = require('telegraf') 
+const cron = require('node-cron');
 const CronJob = require('cron').CronJob;
+
 const generateHour = require('./generateTextHour')
 const {  utcToZonedTime  } = require('date-fns-tz')
 
@@ -8,7 +10,7 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => {
 
-  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.7`)
+  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.8`)
   const tz = 'America/Sao_Paulo'
 
   const job = new CronJob('0 4,9,12,15,18 * * 0-6', function() {
@@ -18,6 +20,15 @@ bot.start((ctx) => {
   }, null, true, tz);
 
   job.start();
+
+
+  cron.schedule('0 4,9,12,15,18 * * 0-7', () => {
+    const currentHour = utcToZonedTime(new Date(), tz).getHours()
+    const currentPrayer = generateHour(currentHour)
+    ctx.reply(`${currentHour}h - rezar ${currentPrayer}`)  }, {
+    scheduled: true,
+    timezone: tz
+  });
 
 }) 
 
