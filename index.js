07819d1f597243b11cfe68aded5b18085c90a88e
Yuji <yujiricardo@gmail.com>
2021-12-08 14:35:50 -0300

    fix: remove useless code

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ const {  utcToZonedTime  } = require('date-fns-tz')
 
 require('dotenv').config()
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN_DEV)
 bot.start((ctx) => {
 
   ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.8`)
   const tz = 'America/Sao_Paulo'
 
-  const job = new CronJob('0 4,9,12,15,18 * * 0-6', function() {
+  new CronJob('0 4,9,12,15,18 * * 0-6', function() {
     const currentHour = utcToZonedTime(new Date(), tz).getHours()
     const currentPrayer = generateHour(currentHour)
     ctx.reply(`${currentHour}h - rezar ${currentPrayer}`)
   }, null, true, tz);
 
-  job.start();
 }) 
 
 bot.launch()
