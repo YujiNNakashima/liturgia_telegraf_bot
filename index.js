3e13fb4fc551c91385cd1cc2e8b05e88917ea59d
Yuji <yujiricardo@gmail.com>
2021-12-08 15:18:37 -0300

    fix: cron

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const generateHour = require('./generateTextHour')
 const {  utcToZonedTime  } = require('date-fns-tz')
 
 require('dotenv').config()
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const tz = 'America/Sao_Paulo'
+let contextChatId;
+
 bot.start((ctx) => {
-
-  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.9`)
-  const tz = 'America/Sao_Paulo'
-
-  new CronJob('0 4,9,12,15,18 * * 0-6', function() {
-    const currentHour = utcToZonedTime(new Date(), tz).getHours()
-    const currentPrayer = generateHour(currentHour)
-    ctx.reply(`${currentHour}h - rezar ${currentPrayer}`)
-  }, null, true, tz);
-
+  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.10`)
+  contextChatId = ctx.chat.id
 }) 
+
+new CronJob('0 4,9,12,15,18 * * 0-6', function() {
+  const currentHour = utcToZonedTime(new Date(), tz).getHours()
+  const currentPrayer = generateHour(currentHour)
+
+  if(contextChatId) {
+    bot.telegram.sendMessage(contextChatId,  `${currentHour}h - rezar ${currentPrayer}`)
+  }
+}, null, true, tz);
 
 bot.launch()
 
