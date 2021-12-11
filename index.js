8302a16190ec0e14a482ed61774fc422afd15455
Kenzonn <lucaskenzonn@gmail.com>
2021-12-11 07:38:33 -0300

    fix hours

--- index.js
+++ index.js
@@ -7,21 +7,19 @@ const {  utcToZonedTime  } = require('date-fns-tz')
 require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const tz = 'America/Sao_Paulo'
-let contextChatId;
 
 bot.start((ctx) => {
-  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.10`)
-  contextChatId = ctx.chat.id
+  ctx.reply(`Olá, ${ctx.from.first_name}, bem-vindo ao bot da liturgia das horas. v1.0.9`)
+  
+  new CronJob('0 4,9,12,15,18 * * 0-6', function() {
+    const currentHour = utcToZonedTime(new Date(), tz).getHours()
+    const currentPrayer = generateHour(currentHour)
+  
+    ctx.reply(`${currentHour}h - rezar ${currentPrayer}`)
+  }, null, true, tz);
+
+  
 }) 
-
-new CronJob('0 4,9,12,15,18 * * 0-6', function() {
-  const currentHour = utcToZonedTime(new Date(), tz).getHours()
-  const currentPrayer = generateHour(currentHour)
-
-  if(contextChatId) {
-    bot.telegram.sendMessage(contextChatId,  `${currentHour}h - rezar ${currentPrayer}`)
-  }
-}, null, true, tz);
 
 bot.launch()
 
